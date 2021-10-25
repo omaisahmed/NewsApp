ebb16b2b8591848fa1ac7047702800d4f5433745
omaisahmed <58178722+omaisahmed@users.noreply.github.com>
2021-10-25 17:11:28 +0500

    25-10-21

---
+++ src/config/rest_consfig.js
@@ -0,0 +1,4 @@
+export const articles_url = 'https://newsapi.org/v2/top-headlines';
+export const country_code = 'in';
+export const category = 'general';
+export const _api_key = '4823b31db18e4f75844a0ed3a1e3562b';
